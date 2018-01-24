93d1d02732b6fe0d41f337e4f0e962d6c4307d1a
ryunosuke <ryunosuke.arima@gmail.com>
2018-01-24 19:19:02 +0900

    [syntax] export したときに optional が動かない不具合を修正

--- src/package/syntax.php
+++ src/package/syntax.php
@@ -71,7 +71,28 @@ function optional($object, $expected = null)
     }
 
     static $nullobject = null;
-    return $nullobject = $nullobject ?: new \ryunosuke\Functions\NullObject();
+    if ($nullobject === null) {
+        // @formatter:off
+        class NullObject implements \ArrayAccess, \IteratorAggregate
+        {
+            public function __isset($name) { return false; }
+            public function __get($name) { return null; }
+            public function __set($name, $value) { throw new \DomainException('called NullObject#' . __FUNCTION__); }
+            public function __unset($name) { throw new \DomainException('called NullObject#' . __FUNCTION__); }
+            public function __call($name, $arguments) { return null; }
+            public function __invoke() { return null; }
+            public function __toString() { return ''; }
+            public function offsetExists($offset) { return false; }
+            public function offsetGet($offset) { return null; }
+            public function offsetSet($offset, $value) { throw new \DomainException('called NullObject#' . __FUNCTION__); }
+            public function offsetUnset($offset) { throw new \DomainException('called NullObject#' . __FUNCTION__); }
+            public function getIterator() { return new \ArrayIterator([]); }
+        }
+        // @formatter:on
+
+        $nullobject = new NullObject();
+    }
+    return $nullobject;
 }
 
 /**
