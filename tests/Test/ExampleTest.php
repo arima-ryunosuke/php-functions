23587582d9d3fc5dee89adcf3e2c25f04ceed048
ryunosuke <ryunosuke.arima@gmail.com>
2018-02-09 23:37:47 +0900

    [tests] テストの変更追従

--- tests/Test/ExampleTest.php
+++ tests/Test/ExampleTest.php
@@ -13,17 +13,18 @@ class ExampleTest extends \ryunosuke\Test\AbstractTestCase
             return;
         }
 
+        require_once __DIR__ . '/../../include/global/function.php';
         defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
         defined('STDOUT') or define('STDOUT', fopen('php://stdout', 'w'));
 
-        foreach (glob(__DIR__ . '/../../src/package/*.php') as $file) {
+        foreach (glob(__DIR__ . '/../../src/Package/*.php') as $file) {
             preg_match_all('#<code>(.*?)</code>.*?function (.+?)\\(#us', file_get_contents($file), $matches, PREG_SET_ORDER);
             $contents = [];
             foreach ($matches as $match) {
-                $contents[] = '//' . $match[2] . preg_replace('#^ \* ?#um', '', $match[1]);
+                $contents[] = '//' . $match[2] . preg_replace('#^     \* ?#um', '', $match[1]);
             }
 
-            $exfile = __DIR__ . '/../example-code/' . basename($file);
+            $exfile = __DIR__ . '/../examples/' . basename($file);
             file_put_contents($exfile, "<?php\n" . implode("\n", $contents));
             ob_start();
             include $exfile;
