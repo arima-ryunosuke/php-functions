3f5902263325fa894683f216ff63f4665c8a8468
ryunosuke <ryunosuke.arima@gmail.com>
2018-04-09 18:01:21 +0900

    ドキュメント生成ツールを apigen から documentize に乗り換え

---
+++ docs/config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'title'      => 'ryunosuke function',
+    'menusize'   => 35,
+    'source-map' => [
+        '.*/' => 'https://github.com/arima-ryunosuke/php-functions/blob/master/src/Package/',
+    ],
+];
