1f1b3e5dba2eedce24b1245db1a1dc5f72f8f382
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-04 22:31:26 +0900

    [feature][Strings] strtr_escaped を追加

--- tests/Test/Package/StringsTest.php
+++ tests/Test/Package/StringsTest.php
@@ -291,6 +291,27 @@ class StringsTest extends AbstractTestCase
         that($found)->isNull();
     }
 
+    function test_strtr_escaped()
+    {
+        // 長いキーから順に置換されるし置換後の文字は置換対象にならない
+        that((strtr_escaped)('XXXX %s %%s %%%%%% %%%%%%%%', [
+            's'    => 'S',
+            '%s'   => 'S',
+            '%%'   => 'P',
+            'XXX'  => 'YYY',
+            'XXXX' => '%s%s',
+        ], '%'))->isSame('%s%s S PS PPP PPPP');
+
+        that((strtr_escaped)('XYZ ab %% %s', [
+            'ab'  => 'AB',  // 2. 1 で置換された文字は対象にならない
+            'A'   => '%a',  // 使われない
+            'Z'   => '%z',  // 使われない
+            '%%'  => 'p',   // 普通に置換される
+            's'   => 'S',   // エスケープが対象なので置換されない（%s は文字 "s" ではない（\n が文字 "n" ではないのと同じ））
+            'XYZ' => 'abc', // 1. 後ろにあるがまず置換される
+        ], '%'))->isSame('abc AB p %s');
+    }
+
     function test_str_bytes()
     {
         that((str_bytes)('abc'))->isSame([97, 98, 99]);
