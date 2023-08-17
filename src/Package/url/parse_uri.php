3afcbfabdf1c97374a1e25c5bdd5311b6a4110fc
ryunosuke <ryunosuke.arima@gmail.com>
2023-08-17 20:30:20 +0900

    [fixbug] parse_uri のスペルミスを修正

--- src/Package/url/parse_uri.php
+++ src/Package/url/parse_uri.php
@@ -94,7 +94,7 @@ function parse_uri($uri, $default = [])
     $parts['user'] = $parts['user'] === null ? null : rawurldecode($parts['user']);
     $parts['pass'] = $parts['pass'] === null ? null : rawurldecode($parts['pass']);
     $parts['host'] = $parts['host'] === null ? null : preg_splice('#^\\[(.+)]$#', '$1', $parts['host']);
-    $parts['path'] = $parts['host'] === null ? null : rawurldecode(concat('/', ltrim($parts['path'], '/')));
+    $parts['path'] = $parts['path'] === null ? null : rawurldecode(concat('/', ltrim($parts['path'], '/')));
     $parts['fragment'] = $parts['fragment'] === null ? null : rawurldecode($parts['fragment']);
 
     if (is_string($parts['query'])) {
